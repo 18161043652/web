4352cbe63cb1ae937efda5e62d9c6de19d2a2a8f
18161043652 <31229221+18161043652@users.noreply.github.com>
2017-08-31 17:50:56 +0800

    17.50

--- Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -26,7 +26,7 @@
 				     	<ul class="menu_lista">
 				   
 				     		
-				     		<?php $__FOR_START_27042__=1;$__FOR_END_27042__=8;for($i=$__FOR_START_27042__;$i < $__FOR_END_27042__;$i+=1){ ?><li>
+				     		<?php $__FOR_START_2652__=1;$__FOR_END_2652__=8;for($i=$__FOR_START_2652__;$i < $__FOR_END_2652__;$i+=1){ ?><li>
 				     			网站首页 
 				     			
 			     				<img src="/Public/images/下拉.png"/>
@@ -194,88 +194,8 @@
 	
 	
 	
-    <!--
-    	作者：兴信网络
-    	时间：2017-08-30
-    	描述：我们的产品
-    -->
-    
-    <style>
-    	.product{
-    		width: 100%;
-			height: auto;
-			border-bottom: 1px solid red;
-			overflow: hidden;
-			background: #f5f5f5;
-			min-height: 300px;
-    	}
-    	.product_heard{
-    		height: 71px;
-    		width: 86%;
-    		margin: auto;
-    		margin-top: 80px;
-    		border: 2px double transparent;
-    	}
-    	.product_hearda{
-    		height: 70px;
-    		width: 16%;
-    		float: left;
-    	}
-    	.product_hearda span:nth-child(1){
-    		display: block;
-    		height: 34px;
-    		font: bold 20px/28px "微软雅黑";
-    		letter-spacing:3px;
-    	}
-    	.product_hearda span div{
-    		display: inline-block;
-    		width: 3rem;
-    		background: #81a405;
-    		height: 3px;
-    		
-    	}
-    	
-    	.product_hearda span:nth-child(2){
-    		display: block;
-    		height: 34px;
-    		font: 400 20px/44px "微软雅黑";
-    		color: #9a9a9a;
-    	}
-    	.product_heardb{
-    		margin-top: 30px;
-    		height: 40px;
-    		width: 76%;
-    		float: left;
-    		overflow: hidden;
-    	}
-    	.product_heardb ul{
-    		font-size: 0px;
-    	}
-    	.product_heardb ul li{
-    		display: inline-block;
-    		height: 38px;
-    		font:400 16px/38px "微软雅黑";
-    		padding: 0px 20px;
-    		margin: 0px 10px;
-    		border: 1px solid #dcdcdc;
-    		cursor: pointer;
-    		color: gray;
-    		transition: all 1s;
-    		overflow: hidden;
-    	}
-    	
-    	.product_heardb ul li:hover{
-    		background: #252f03;
-    		color: whitesmoke;
-    	}
-    	.product_box{
-    		height:400px;
-    		width: 86%;
-    		margin: auto;
-    		margin-top: 80px;
-    		border: 2px double teal;
-    	}
-    </style>
+    <!--描述：我们的产品-->
+
 	<div class="product">
 		<div class="product_heard">
 			<div class="product_hearda">
@@ -285,24 +205,197 @@
 			
 			<div class="product_heardb">
 				<ul>
-					<?php $__FOR_START_1587__=1;$__FOR_END_1587__=11;for($i=$__FOR_START_1587__;$i < $__FOR_END_1587__;$i+=1){ ?><li>产品名称 <?php echo ($i); ?></li><?php } ?>
+					<?php $__FOR_START_2446__=1;$__FOR_END_2446__=11;for($i=$__FOR_START_2446__;$i < $__FOR_END_2446__;$i+=1){ ?><li>产品名称 <?php echo ($i); ?></li><?php } ?>
 				</ul>
 			</div>
 		</div>
 	
 		<div class="product_box">
-			
+				
+			<div id="product_in">
+				<?php $__FOR_START_5435__=1;$__FOR_END_5435__=10;for($i=$__FOR_START_5435__;$i < $__FOR_END_5435__;$i+=1){ ?><div class="product_show">
+						<div class="product_show_img">
+							<img src="/Public/images/product.jpg"/>
+							<div class="product_hiden">
+								<img src="/Public/images/加.png" onclick="alert()"/>
+							</div>
+						</div>
+						
+						<div class="product_show_text">
+							草鸡藤椒油<?php echo ($i); ?>  &nbsp;&nbsp;250ml
+						</div>
+					</div><?php } ?>
+				<!--<div style="clear: both;"></div>-->
+			</div>
 		</div>
+	
+		<div class="product_btn">
+			<div class="product_btna">
+				<img src="/Public/images/pagea.png" class="pagep"/>
+				<img src="/Public/images/pageo.png"/>
+				<img src="/Public/images/pagen.png" class="pagen"/>
+			</div>
+		</div>
+		
+	
+	
 	</div>
 	
-	<div style="height: 2000px">
+	<!--商品展示-->
+	
+	<style>
+    	.news_heardb{
+    		margin-top: 30px;
+    		height: 40px;
+    		width: 76%;
+    		float: left;
+    		overflow: hidden;
+    	}
+   
+    	.news_heardb ul{
+    		font-size: 0px;
+    	}
+    	.news_heardb ul li{
+    		display: inline-block;
+    		height: 38px;
+    		font:400 16px/38px "微软雅黑";
+    		padding: 0px 20px;
+    		margin: 0px 10px;
+    		border: 1px solid #dcdcdc;
+    		background: white;
+    		
+    		cursor: pointer;
+    		color: gray;
+    		transition: all 1s;
+    		overflow: hidden;
+    		
+    	}
+    	.news_heardb ul li:hover{
+    		background: #252f03;
+    		color: white;
+    		border: 1px solid #252f03;
+    	}
+		.new_box{
+			width: 86%;
+    		margin: auto;
+    		margin-top: 80px;
+    		overflow: hidden;
+    		display: flex;
+    		display: -webkit-flex;
+    		flex-flow: row wrap;
+  			align-content: flex-start;
+		}
 		
-	</div>
+		.new_box>div {
+		  box-sizing: border-box;
+		  flex: 0 0 25%;
+		  border: 1px solid transparent;
+		}
+		.news_show{
+			width: 90%;
+			height: 86%;
+			background: gainsboro;
+		}
+		.news_img{
+			height: 84%;
+			background: gold;
+		}
+		.news_text{
+			height: 15%;
+			text-align: right;
+			font: 400 14px/3 "微软雅黑";
+		}
+    </style>
+    <div class="news">
+		<div class="product_heard">
+			<div class="product_hearda">
+				<span style="color: white;">新闻中心   <div></div></span>
+				<span style="color: white;">用心甄选  &nbsp;&nbsp;&nbsp;只为品质</span>
+			</div>
+			
+			<div class="news_heardb">
+				<ul>
+					<?php $__FOR_START_3481__=1;$__FOR_END_3481__=9;for($i=$__FOR_START_3481__;$i < $__FOR_END_3481__;$i+=1){ ?><li>产品名称 <?php echo ($i); ?></li><?php } ?>
+				</ul>
+			</div>
+		</div>
+	
+		<div class="new_box">
+				<?php $__FOR_START_27842__=0;$__FOR_END_27842__=8;for($i=$__FOR_START_27842__;$i < $__FOR_END_27842__;$i+=1){ ?><div>
+						<div class="news_show">
+							<div class="news_img">!</div>
+							
+							
+							<div class="news_text">
+								擦看更多 &nbsp;&nbsp;&nbsp;&nbsp;
+							</div>
+						</div>
+					</div><?php } ?>
+			
+		</div>
+	
+		<div class="product_btn">
+			<div class="product_btna">
+				<img src="/Public/images/pagea.png" class="pagep"/>
+				<img src="/Public/images/pageo.png"/>
+				<img src="/Public/images/pagen.png" class="pagen"/>
+			</div>
+		</div>
+    
+    
+
 	
 	<script>
 		
 		$(function(){
-	
+			
+			
+			
+			$(".product_hiden").width($(".product_show_img").width());
+			
+			
+			
+				$(".new_box>div").height($(".new_box>div").width()*0.75);
+			
+			
+			//商品切换
+			
+			var num=	 $("#product_in").find(".product_show").size();
+			
+			console.log(num);
+				
+				
+			 $("#product_in").append($("#product_in").clone());
+
+			var  i=0;
+			$(".pagen").click(function(){
+				i++;
+				if (i==num+1) {
+					$(".product_box").scrollLeft(0);
+						i=1;
+				}
+					$(".product_box").animate({scrollLeft:i*326});
+				
+				console.log(i);
+				
+			})
+			
+			$(".pagep").click(function(){
+			
+				i--;
+				
+				if (i<0) {
+					$(".product_box").scrollLeft((num)*326);
+					i=num-1;
+				}
+				
+				console.log(i);
+				$(".product_box").animate({scrollLeft:i*326});
+				
+					
+				
+			})
+			
 		})
 	
 	</script>
@@ -314,8 +407,269 @@
 		
 		
 		
+	
+		
+			
+			<!--友情连接-->
+			<div class="connection">
+				
+				<div class="connection_box">
+					
+				
+						<div class="product_hearda">
+							<span style="color: white;">友情连接   <div></div></span>
+							<span style="color: white;">用心甄选  &nbsp;&nbsp;&nbsp;只为品质</span>
+						</div>
+						
+						<div class="connection_heardb">
+							<ul>
+								<?php $__FOR_START_26410__=1;$__FOR_END_26410__=5;for($i=$__FOR_START_26410__;$i < $__FOR_END_26410__;$i+=1){ ?><li>
+										/ <img src="/Public/images/QQ截图20170831150808.jpg"/> 
+									</li><?php } ?>
+							</ul>
+						</div>
+					
+				</div>
+			</div>
+			
+			<!--联系我们-->
+			
+			
+			
+			
+			<div class="countus">
+				<!--；联系方式-->
+				<div>
+					<div  class="count_h">
+						联系我们
+					</div>
+					<div  class="count_n">
+						四川梓州农业有限公司
+					</div>
+					<div  class="count_t">
+						<img src="/Public/images/地址.png"/> 地址：绵阳市崇上国际11楼4号
+					</div>
+					<div  class="count_t">
+						<img src="/Public/images/电话 (1) - 副本.png"/> 电话：13888881234
+					</div>
+					<div  class="count_t">
+						<img src="/Public/images/邮箱 (1).png"/> 邮箱：1321321321@live.com
+					</div>
+					<div  class="count_t">
+						<img src="/Public/images/工具-邮编.png"/> 邮编：321321
+					</div>
+				</div>
+				
+				<!--二维码-->
+				<div>
+					
+					<div  class="count_h">
+						关注我们
+					</div>
+					
+					<div class="count_img">
+						<div>
+							<img src="/Public/images/QQ截图20170831160319.jpg"/>
+							
+							<div class="count_img_t">
+								移动版官网
+							</div>
+						</div>
+						<div>
+							<img src="/Public/images/QQ截图20170831160319.jpg"/>
+							
+							<div class="count_img_t">
+								移动版官网
+							</div>
+						</div>
+					</div>
+					
+				</div>
+				<!--表单-->
+				<div >
+					
+					<div class="count_forma"></div>
+					
+					<div class="count_formb">
+						
+						<div class="count_formb_a">
+							我们只为你而想
+						</div>
+						<div class="count_formb_b">
+							我们只为你而想
+						</div>
+						<div class="count_formb_c">
+							<input type="" name="" id="" value="" placeholder="name"/>
+						</div>
+						
+						<div class="count_formb_c">
+							<input type="" name="" id="" value="" placeholder="tell"/>
+						</div>
+						
+						<div class="count_formb_c">
+							<input type="" name="" id="" value="" placeholder="emial"/>
+						</div>
+						
+						<div class="count_formb_c">
+							<textarea type="" name="" id="" value="" >请输入你的问题</textarea>
+						</div>
+						
+						<div class="count_formb_d">
+							确认提交
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			
+			<!--底部-->
+			<style>
+	
+				.footer{
+					margin-top: 15px;
+					width: 100%;
+					height: 50px;
+					background: #252f03;
+					position: absolute;
+				}
+				.footer_in{
+					width: 86%;
+					margin:auto;
+					height: 100%;
+					overflow: hidden;
+					line-height: 50px;
+				}
+				.footer_lefta{
+					padding: 0px 20px;
+					float: left;
+					height: 100%;
+					background: #7ea509;
+					color: #252f03;
+				}
+				
+				.footer_leftb{
+					padding: 0px 20px;
+					float: left;
+					height: 100%;
+					color: white;
+					font-weight: 600;
+				}
+				.footer_leftb span{cursor: pointer;}
+				
+				.footer_right{
+					padding: 0px 20px;
+					float: right;
+					height: 100%;
+					text-align: right;		
+					color: #80a700;			
+				}
+				
+				
+				#weixing{
+					opacity:0;
+					-webkit-transform:translateY(300px);
+					transform:translateY(300px);
+				}
+				
+				#txqq{
+					opacity:0;
+					-webkit-transform:translateY(300px);
+					transform:translateY(300px);
+					
+				}
+				
+				.qqweixshow{
+					-webkit-animation:fadeInUpBig .6s  ease both;;
+					animation:fadeInUpBig .6s  ease both;;
+				}
+					@-webkit-keyframes fadeInUpBig{
+						0%{
+							opacity:0;
+							position: absolute;
+							right: 100px;
+							webkit-transform:translateY(300px);
+						}
+						100%{
+							opacity:1;
+							-webkit-transform:translateY(-250px);
+							position: absolute;
+							right:100px;
+						}
+					}
+					@keyframes fadeInUpBig{
+						0%{
+							opacity:0;
+							position: absolute;
+							right: 100px;
+							transform:translateY(300px);
+						}
+						100%{
+							opacity:1;
+							transform:translateY(-250px);
+							position: absolute;
+							right:100px;
+							}
+					}
+			</style>
+			<div class="footer">
+				<div class="footer_in">
+					<div class="footer_lefta">
+						友情连接
+					</div>
+					
+					<div class="footer_leftb">
+						<span>兴信网络</span>/<span>梓州农业</span> 
+					</div>
+					
+					<div class="footer_right">
+						<span>版权所有  © 2018-2020 川ICP备8888888  </span> &nbsp;&nbsp;&nbsp;&nbsp;
+						<span>  技术支持：兴信网络 </span>&nbsp;&nbsp;&nbsp;
+						<span id="qqshow">
+							<img src="/Public/images/QQ.png"/>
+							
+						</span>&nbsp;|&nbsp;
+						
+						<span id="wxshow"><img src="/Public/images/微信.png"/></span>
+						
+						
+					</div>
+					
+					<img id="weixing" src="/Public/images/QQ截图20170831160319.jpg"/>
+					<img id="txqq" src="/Public/images/QQ截图20170831160319.jpg"/>
+					
+				</div>
+				
+				
+				
+			</div>
+		
+		
 		
 		
 		
 	</body>
+	
+	<script>
+		$(function(){
+			
+			var counth=$(".count_formb").width();
+			$(".count_formb").height(counth*0.803);
+			
+			
+			$('#qqshow').hover(function(){
+				$("#txqq").addClass('qqweixshow');
+			},function(){
+				
+				$("#txqq").removeClass('qqweixshow');
+			})
+			
+			$('#wxshow').hover(function(){
+				$("#weixing").addClass('qqweixshow');
+			},function(){
+				
+				$("#weixing").removeClass('qqweixshow');
+			})
+			
+		})
+	</script>
 </html>
